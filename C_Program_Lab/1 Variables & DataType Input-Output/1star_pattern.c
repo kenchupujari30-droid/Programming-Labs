de980828cb6ddb7774646dbebb903ef47558d91a
Kenchu Pujari <kenchupujari30@gmail.com>
2026-07-09 18:41:41 +0530

    Added loop programs

---
+++ C_Program_Lab/1 Variables & DataType Input-Output/1star_pattern.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Right Half pyramid\n");
+  printf("*\n");
+  printf("* *\n");
+  printf("* * *\n");
+  printf("* * * * \n");
+  printf("* * * * *\n");
+
+}
---
+++ C_Program_Lab/2Instructions_Expressions & Operators/1Arithmetic_Operators.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+int a=20 , b=30;
+
+printf("Addition =%d\n " , a + b);
+printf("Substration =%d\n " ,a - b);
+printf("Multiplication =%d\n ",a * b);
+printf("Division =%d\n ",a / b);
+printf("Module =%d\n ",a % b);
+
+
+
+}
